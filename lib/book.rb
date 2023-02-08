1180eba00b67813db05cd79578c5cc42682f927b
mknyambura <kariuki.mercie@gmail.com>
2023-02-08 12:26:08 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    attr_accessor :title, :page_count, :genre, :author
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
